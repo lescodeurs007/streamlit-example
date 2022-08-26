4511b3db4af5708596a1fea94de9a28b0fffd7fe
lescodeurs007 <111956012+lescodeurs007@users.noreply.github.com>
2022-08-26 08:56:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,4 @@
-import streamlit.components.v1 as components
-
-# Declare the component:
-my_component = components.declare_component("my_component", path="frontend/build")
-
-# Use it:
-my_component(greeting="Hello", name="World")
-class MyComponent extends StreamlitComponentBase {
-    public render(): ReactNode {
-        // Access arguments from Python via `this.props.args`:
-        const greeting = this.props.args["greeting"]
-        const name = this.props.args["name"]
-        return <div>{greeting}, {name}!</div>
-    }
-}
+from PIL import Image
+import streamlit as st
+image = Image.open('sunrise.jpg')
+st.image(image, caption='Sunrise by the mountains')
